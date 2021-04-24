546711033f53df3cece60ddc0a7205ea9593ac84
Sami daoud <s.m.d4321@gmail.com>
2021-04-25 00:55:50 +0300

    functional components

--- src/components/DishDetailsComponent.js
+++ src/components/DishDetailsComponent.js
@@ -1,65 +1,65 @@
-import { Component } from 'react'
+import React from 'react'
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-class DishDetails extends Component {
-    constructor(props) {
-        super(props)
-        console.log(" DishDetails constructor")
-    }
-    renderComments(comments) {
-        if (comments != null) {
-            const listedComments = comments.map((comment) => {
-                return (
-                    <div key={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p>--{comment.author},{new Intl.DateTimeFormat('en-Us', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                    </div>
-                );
-            });
+function RenderDish({ dish }) {
+    return (
+        <div key={dish.id} className='col-12 col-md-5 m-1'>
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
 
-
+function RenderComments({comments}) {
+    if (comments != null) {
+        const listedComments = comments.map((comment) => {
             return (
+                <div key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>--{comment.author},{new Intl.DateTimeFormat('en-Us', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                </div>
+            );
+        });
+
+        return (
+            <div className='col-12 col-md-5 m-1'>
+                <h1>Comments</h1>
                 <div className="list-unstyled">
-                    { listedComments}
+                    {listedComments}
                 </div>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
+            </div>
+        );
     }
-    render() {
-        const dish = this.props.dish
-        console.log(dish)
-        if (dish != null) {
-
-            return (
-                <div className='container'>
-                    <div className='row'>
-                        <div key={dish.id} className='col-12 col-md-5 m-1'>
-                            <Card>
-                                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                                <CardBody>
-                                    <CardTitle>{dish.name}</CardTitle>
-                                    <CardText>{dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-                        <div className='col-12 col-md-5 m-1'>
-                            <h1>Comments</h1>
-                            {this.renderComments(dish.comments)}
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-        else {
-            return (
-                <div> </div>
-            );
-        }
+    else {
+        return (
+            <div></div>
+        );
     }
 }
+
+
+const DishDetails = (props) => {
+    const dish = props.dish
+    if (dish != null) {
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <RenderDish dish={dish} />
+                    <RenderComments comments={dish.comments} />                    
+                </div>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div> </div>
+        );
+    }
+}
+
 export default DishDetails;
